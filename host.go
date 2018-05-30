324e336a83c1f3f6fdb571b56663877de5722004
kent <kentkawashima@gmail.com>
2018-05-30 15:41:56 +0900

    Refactors Host to accomodate IntrahostModel

--- host.go
+++ host.go
@@ -3,122 +3,99 @@ package contagiongo
 import (
 	"fmt"
 	"sort"
-	"strings"
 )
 
 // Host encapsulates pathogens together and ties its evolution to a particular
 // set of parameters given by its assigned host type.
 type Host interface {
-	// HostID returns the unique ID of the host.
-	HostID() int
-
-	HostTypeID() int
-
-	// Pathogen returns one pathogen from a specific host. Returns nil
-	// if no pathogen exists in the specified position.
+	// ID returns the unique ID of the host.
+	ID() int
+	// TypeID returns the ID representing the host's type in a multi-host
+	// simulation. Generally, the host type ID is used to identify hosts
+	// belonging to the same group that share the same properties.
+	TypeID() int
+	// Pathogen returns one pathogen from the current host based on
+	// its given position in the list of pathogens.
+	// Returns nil if no pathogen exists in the specified position.
 	Pathogen(i int) interface{}
-
-	// Pathogens returns all the pathogens present in the host.
+	// Pathogens returns a list of all pathogens present in the host.
+	// This elements of the list are pointers to SequenceNodes or
+	// PathogenNodes (not implemented).
 	Pathogens() []interface{}
-
 	// PathogenPopSize returns the number of pathogens inside the host.
 	PathogenPopSize() int
-
 	// AddPathogen appends a pathogen to the pathogen space of the host.
 	// Returns the new pathogen population size.
 	AddPathogen(p interface{}) int
-
-	// RemovePathogensByID removes pathogens based on the list of IDs given.
+	// RemovePathogens removes pathogens based on the list of positions given.
 	// Returns the number of pathogens remaining and any errors encountered.
-	RemovePathogensByID(ids ...int) (n int, err error)
-
-	// // Returns a deep copy of the host, including all the pathogens
-	// // present.
-	// Clone() Host
-
-	// ClearPathogens removes all the pathogens from the pathogen space
-	// of the host, leaving an empty list.
-	ClearPathogens()
-
+	RemovePathogens(ids ...int) (n int, err error)
+	// RemoveAllPathogens removes all the pathogens from the host.
+	// Internally, this removes all the pointers that refer to SequenceNodes
+	// or PathogenNodes.
+	RemoveAllPathogens()
 	// DecrementTimer decreases the internal timer by 1.
 	DecrementTimer()
-
-	// SetModel sets the type of spreader, Replicator, mutator, and fitness to
-	// be used by the host.
-	SetModel(name string, model interface{}) error
+	// SetModel associates the current host to a given intrahost model.
+	// The intrahost model governs intrahost processes by specifying the
+	// mutation, replication, recombination, and infection modes and parameters
+	// to be used.
+	SetIntrahostModel(intrahostModel IntrahostModel) error
 }
 
-type EpidemicHost interface {
-	Host
-	Spreader
-	Replicator
-	Mutator
+type sequenceHost struct {
+	id             int
+	typeID         int
+	intrahostModel IntrahostModel
+	internalTimer  int
+	pathogens      []SequenceNode
 }
 
-// SequenceHost is type of host that implements the Host interface and embeds
-// a Spreader, Replicator, Mutator, and SequenceFitness.
-// The SequenceHost is identified by its hostID and can be accessed using the
-// HostID method.
-// Pathogens associated with the host are listed under the pathogen property
-// and can be accessed using the Pathogen or Pathogens methods.
-// Pathogens can be added into the host using the AddPathogen method and can
-// be removed by pathogenID using the RemovePathogensByID method. All pathogens
-// can be removed fromt the host using the ClearPathogens method.
-type SequenceHost struct {
-	Spreader
-	Replicator
-	Mutator
-	SequenceFitness
-
-	hostID        int
-	pathogens     []SequenceNode
-	internalTimer int
-}
-
-// NewEmptySequenceHost creates a new host without spreader, replicator, and
-// mutator features. It also has no status time intervals and
-// lacks any pathogens.
-func NewEmptySequenceHost(id int) *SequenceHost {
-	h := new(SequenceHost)
-	h.hostID = id
+// NewEmptySequenceHost creates a new host without an intrahost model and
+// no pathogens.
+func NewEmptySequenceHost(ids ...int) Host {
+	h := new(sequenceHost)
+	h.id = ids[0]
+	h.typeID = 0 // default
+	if len(ids) > 1 {
+		h.typeID = ids[1]
+	}
+	h.intrahostModel = nil
+	h.internalTimer = -1
+	h.pathogens = []SequenceNode{}
 	return h
 }
 
-// HostID returns the unique ID of the host.
-func (h *SequenceHost) HostID() int {
-	return h.hostID
+func (h *sequenceHost) ID() int {
+	return h.id
 }
 
-// Pathogen returns one pathogen from a specific host. Returns nil
-// if no pathogen exists in the specified position.
-func (h *SequenceHost) Pathogen(i int) interface{} {
+func (h *sequenceHost) TypeID() int {
+	return h.typeID
+}
+
+func (h *sequenceHost) Pathogen(i int) interface{} {
 	return h.pathogens[i]
 }
 
-// Pathogens returns all the pathogens present in the host.
-func (h *SequenceHost) Pathogens() []interface{} {
-	var pathogens []interface{}
-	for _, p := range h.pathogens {
-		pathogens = append(pathogens, p)
+func (h *sequenceHost) Pathogens() []interface{} {
+	pathogens := make([]interface{}, len(h.pathogens))
+	for i, p := range h.pathogens {
+		pathogens[i] = p
 	}
 	return pathogens
 }
 
-// PathogenPopSize returns the number of pathogens inside the host.
-func (h *SequenceHost) PathogenPopSize() int {
+func (h *sequenceHost) PathogenPopSize() int {
 	return len(h.pathogens)
 }
 
-// AddPathogen appends a pathogen to the pathogen space of the host.
-// Returns the new pathogen population size.
-func (h *SequenceHost) AddPathogen(p interface{}) int {
+func (h *sequenceHost) AddPathogen(p interface{}) int {
 	h.pathogens = append(h.pathogens, p.(SequenceNode))
 	return len(h.pathogens)
 }
 
-// RemovePathogensByID removes pathogens based on the list of IDs given.
-// Returns the number of pathogens remaining and any errors encountered.
-func (h *SequenceHost) RemovePathogensByID(ids ...int) (n int, err error) {
+func (h *sequenceHost) RemovePathogensByID(ids ...int) (n int, err error) {
 	sort.Ints(ids)
 	// Check if the largest ID is less than the number of pathogens
 	lastID := ids[len(ids)-1]
@@ -136,31 +113,20 @@ func (h *SequenceHost) RemovePathogensByID(ids ...int) (n int, err error) {
 	return len(h.pathogens), nil
 }
 
-// ClearPathogens removes all the pathogens from the pathogen space
-// of the host, leaving an empty list.
-func (h *SequenceHost) ClearPathogens() {
-	h.pathogens = []SequenceNode{}
+func (h *sequenceHost) RemoveAllPathogens() {
+	for i := range h.pathogens {
+		h.pathogens[i] = nil
+	}
+	h.pathogens = h.pathogens[:0]
 }
 
-// DecrementTimer decreases the internal timer by 1.
-func (h *SequenceHost) DecrementTimer() {
+func (h *sequenceHost) DecrementTimer() {
 	h.internalTimer--
 }
 
-// SetModel sets the type of spreader, Replicator, mutator, and fitness to
-// be used by the host.
-func (h *SequenceHost) SetModel(keyword string, model interface{}) error {
-	switch strings.ToLower(keyword) {
-	case "spreader":
-		h.Spreader = model.(Spreader)
-	case "Replicator":
-		h.Replicator = model.(Replicator)
-	case "mutator":
-		h.Mutator = model.(Mutator)
-	case "landscape":
-		h.SequenceFitness = model.(SequenceFitness)
-	default:
-		return fmt.Errorf("unknown keyword")
+func (h *sequenceHost) SetModel(intrahostModel IntrahostModel) error {
+	if h.intrahostModel != nil {
+		return fmt.Errorf(IntrahostModelExistsError, h.intrahostModel.Name(), h.intrahostModel.ID())
 	}
-	return nil
+	h.intrahostModel = intrahostModel
 }
