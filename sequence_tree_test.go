d25efb8f6892e3c50a00d0025b90f6d9256bda89
kent <kentkawashima@gmail.com>
2018-05-30 22:03:41 +0900

    Adds tests to Genotype structs and comments out old tests

--- sequence_tree_test.go
+++ sequence_tree_test.go
@@ -1,141 +1,204 @@
 package contagiongo
 
-import (
-	"fmt"
-	"testing"
+import "testing"
 
-	"github.com/segmentio/ksuid"
-)
-
-func TestNewSequenceTree_panic(t *testing.T) {
+func TestNewGenotype(t *testing.T) {
 	defer func() {
 		if err := recover(); err != nil {
-			t.Fatalf(UnexpectedErrorWhileError, "calling NewSequenceTree constructor", err)
+			t.Fatalf(UnexpectedErrorWhileError, "calling NewGenotype constructor", err)
 		}
 	}()
-	rootSequence := []int{0, 1, 0, 1, 0, 1, 0, 1, 0, 1}
-	NewSequenceTree(rootSequence)
+	sequence := []int{0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1}
+	NewGenotype(sequence)
 }
 
-func TestNewSequenceTree(t *testing.T) {
+func TestEmptyGenotypeSet(t *testing.T) {
 	defer func() {
 		if err := recover(); err != nil {
-			t.Fatalf(UnexpectedErrorWhileError, "calling NewSequenceTree constructor", err)
+			t.Fatalf(UnexpectedErrorWhileError, "calling EmptyGenotypeSet constructor", err)
 		}
 	}()
-	rootSequence := []int{0, 1, 0, 1, 0, 1, 0, 1, 0, 1}
-	tree := NewSequenceTree(rootSequence)
-	sequenceTree := tree.(*sequenceTree)
+	EmptyGenotypeSet()
+}
 
-	if l := len(sequenceTree.roots); l != 1 {
-		t.Errorf(UnequalIntParameterError, "number of roots", 1, l)
+func sampleGenotype() *Genotype {
+	sequence := []int{0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1}
+	g := NewGenotype(sequence)
+	return g
+}
+
+func TestGenotypeSet_Add(t *testing.T) {
+	g := sampleGenotype()
+	set := EmptyGenotypeSet()
+	set.Add(g)
+	if l := set.Size(); l != 1 {
+		t.Errorf(UnequalIntParameterError, "size of genotype set", 1, l)
 	}
-	for _, root := range sequenceTree.roots {
-		if fmt.Sprintf("%v", root.sequence) != fmt.Sprintf("%v", rootSequence) {
-			t.Errorf(UnequalStringParameterError, "integer sequence", fmt.Sprintf("%v", rootSequence), fmt.Sprintf("%v", root.sequence))
+	set.Add(g)
+	if l := set.Size(); l != 1 {
+		t.Errorf(UnequalIntParameterError, "size of genotype set", 1, l)
+	}
+}
+
+func sampleSequenceShort() []int {
+	sequence := []int{0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1}
+	return sequence
+}
+
+func TestGenotypeSet_AddSequence(t *testing.T) {
+	sequence := sampleSequenceShort()
+	set := EmptyGenotypeSet()
+	g := set.AddSequence(sequence)
+	if l := set.Size(); l != 1 {
+		t.Errorf(UnequalIntParameterError, "size of genotype set", 1, l)
+	}
+	g = set.AddSequence(sequence)
+	if l := set.Size(); l != 1 {
+		t.Errorf(UnequalIntParameterError, "size of genotype set", 1, l)
+	}
+}
+
+func TestGenotypeSet_Remove(t *testing.T) {
+	sequence := sampleSequenceShort()
+	set := EmptyGenotypeSet()
+	g := set.AddSequence(sequence)
+
+	set.Remove(sequence)
+	if l := set.Size(); l != 0 {
+		t.Errorf(UnequalIntParameterError, "size of genotype set", 0, l)
+	}
+}
+
+func TestNewGenotypeNode(t *testing.T) {
+	defer func() {
+		if err := recover(); err != nil {
+			t.Fatalf(UnexpectedErrorWhileError, "calling NewGenotype constructor", err)
 		}
-	}
+	}()
+	sequence := sampleSequenceShort()
+	set := EmptyGenotypeSet()
+	NewGenotypeNode(sequence, set)
 }
 
-func sampleSequenceTree() *sequenceTree {
-	rootSequence := []int{
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-	}
-	tree := NewSequenceTree(rootSequence)
-	return tree.(*sequenceTree)
-}
+// func TestNewSequenceTree(t *testing.T) {
+// 	defer func() {
+// 		if err := recover(); err != nil {
+// 			t.Fatalf(UnexpectedErrorWhileError, "calling NewSequenceTree constructor", err)
+// 		}
+// 	}()
+// 	rootSequence := []int{0, 1, 0, 1, 0, 1, 0, 1, 0, 1}
+// 	tree := NewSequenceTree(rootSequence)
+// 	sequenceTree := tree.(*sequenceTree)
 
-func TestSequenceTree_NewSub(t *testing.T) {
-	tree := sampleSequenceTree()
-	var key ksuid.KSUID
-	for k := range tree.pathogens {
-		key = k
-	}
-	parent := tree.Sequence(key)
-	if parent == nil {
-		t.Fatalf(UnexpectedErrorWhileError, "getting parent", "nil")
-	}
-	position := 0
-	state := 1
-	child := tree.NewSub(parent, position, state)
+// 	if l := len(sequenceTree.roots); l != 1 {
+// 		t.Errorf(UnequalIntParameterError, "number of roots", 1, l)
+// 	}
+// 	for _, root := range sequenceTree.roots {
+// 		if fmt.Sprintf("%v", root.sequence) != fmt.Sprintf("%v", rootSequence) {
+// 			t.Errorf(UnequalStringParameterError, "integer sequence", fmt.Sprintf("%v", rootSequence), fmt.Sprintf("%v", root.sequence))
+// 		}
+// 	}
+// }
 
-	if l := len(tree.pathogens); l != 2 {
-		t.Errorf(UnequalIntParameterError, "number of pathgoens", 2, l)
-	}
+// func sampleSequenceTree() *sequenceTree {
+// 	rootSequence := []int{
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 		0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
+// 	}
+// 	tree := NewSequenceTree(rootSequence)
+// 	return tree.(*sequenceTree)
+// }
 
-	if res := child.Parents(); res[0] != parent {
-		t.Errorf(UnequalStringParameterError, "parent sequence pointer", fmt.Sprintf("%p", res), fmt.Sprintf("%p", parent))
-	}
-	if res := child.Sequence()[position]; res != state {
-		t.Errorf(UnequalIntParameterError, "state at position 0", state, res)
-	}
-	// TODO: Add test to check if keys duplicate
-}
+// func TestSequenceTree_NewSub(t *testing.T) {
+// 	tree := sampleSequenceTree()
+// 	var key ksuid.KSUID
+// 	for k := range tree.pathogens {
+// 		key = k
+// 	}
+// 	parent := tree.Sequence(key)
+// 	if parent == nil {
+// 		t.Fatalf(UnexpectedErrorWhileError, "getting parent", "nil")
+// 	}
+// 	position := 0
+// 	state := 1
+// 	child := tree.NewSub(parent, position, state)
 
-func TestSequenceTree_NewRoot(t *testing.T) {
-	tree := sampleSequenceTree()
-	rootSequence := []int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
-	tree.NewRoot(rootSequence)
+// 	if l := len(tree.pathogens); l != 2 {
+// 		t.Errorf(UnequalIntParameterError, "number of pathgoens", 2, l)
+// 	}
 
-	if l := len(tree.roots); l != 2 {
-		t.Errorf(UnequalIntParameterError, "number of roots", 2, l)
-	}
+// 	if res := child.Parents(); res[0] != parent {
+// 		t.Errorf(UnequalStringParameterError, "parent sequence pointer", fmt.Sprintf("%p", res), fmt.Sprintf("%p", parent))
+// 	}
+// 	if res := child.Sequence()[position]; res != state {
+// 		t.Errorf(UnequalIntParameterError, "state at position 0", state, res)
+// 	}
+// 	// TODO: Add test to check if keys duplicate
+// }
 
-	var key ksuid.KSUID
-	for k := range tree.pathogens {
-		key = k
-	}
-	if fmt.Sprintf("%v", tree.roots[key].sequence) != fmt.Sprintf("%v", rootSequence) {
-		t.Errorf(UnequalStringParameterError, "integer sequence", fmt.Sprintf("%v", rootSequence), fmt.Sprintf("%v", tree.roots[key].sequence))
-	}
-	// TODO: Add test to check if keys duplicate
-}
+// func TestSequenceTree_NewRoot(t *testing.T) {
+// 	tree := sampleSequenceTree()
+// 	rootSequence := []int{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+// 	tree.NewRoot(rootSequence)
+
+// 	if l := len(tree.roots); l != 2 {
+// 		t.Errorf(UnequalIntParameterError, "number of roots", 2, l)
+// 	}
+
+// 	var key ksuid.KSUID
+// 	for k := range tree.pathogens {
+// 		key = k
+// 	}
+// 	if fmt.Sprintf("%v", tree.roots[key].sequence) != fmt.Sprintf("%v", rootSequence) {
+// 		t.Errorf(UnequalStringParameterError, "integer sequence", fmt.Sprintf("%v", rootSequence), fmt.Sprintf("%v", tree.roots[key].sequence))
+// 	}
+// 	// TODO: Add test to check if keys duplicate
+// }
