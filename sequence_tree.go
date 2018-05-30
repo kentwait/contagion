4e39ca69e5cb80f7d4d7706cb44326dc082b3a7c
kent <kentkawashima@gmail.com>
2018-05-30 13:44:45 +0900

    Refactors SequenceNode and adds History method

--- sequence_tree.go
+++ sequence_tree.go
@@ -3,16 +3,97 @@ package contagiongo
 import (
 	"fmt"
 	"sync"
+
+	"github.com/segmentio/ksuid"
 )
+
+// SequenceNode represents a sequence genotype in the sequence tree.
+type SequenceNode interface {
+	// ID returns the unique ID of the pathogen node.
+	ID() int
+	// Parent returns the parent of the node.
+	Parent() SequenceNode
+	// Children returns the children of the node.
+	Children() []SequenceNode
+	// Sequence returns the sequence of the current node.
+	Sequence() []int
+	// Fitness returns the fitness value of this node based on its current
+	// sequence and the given fitness model. If the fitness of the node has
+	// been computed before using the same fitness model, then the value is
+	// returned from memory and is not recomputed.
+	Fitness(landscape SequenceFitness) float64
+	// NumSites returns the number of sites being modeled in this pathogen node.
+	NumSites() int
+	// StateCounts returns the number of sites by state, postion corresponds to the state from 0 to n.
+	StateCounts() map[int]int
+	// History returns the list of sequences that resulted into the extant
+	// sequence.
+	History(h [][]int) [][]int
+}
+
+type sequenceNode struct {
+	uid      ksuid.KSUID
+	parents  []SequenceNode
+	children []SequenceNode
+	// subs        map[int][]int             // key the position of the substitution, value is list of new states. Essentially tracks if multiple hits occured
+	// recombs     map[int][][]*sequenceNode // key is the position of recombination, value is a list of sequence node pairs representing its parents
+	sequence    []int
+	stateCounts map[int]int     // key is the state
+	fitness     map[int]float64 // key is the fitness model id
+}
+
+func (n *sequenceNode) ID() ksuid.KSUID {
+	return n.uid
+}
+
+func (n *sequenceNode) Parents() []SequenceNode {
+	return n.parents
+}
+
+func (n *sequenceNode) Children() []SequenceNode {
+	return n.children
+}
+
+func (n *sequenceNode) Sequence() []int {
+	return n.sequence
+}
+
+func (n *sequenceNode) Fitness(f SequenceFitness) float64 {
+	id := f.ID()
+	if _, ok := n.fitness[id]; !ok {
+		fitness, _ := f.Fitness(n.sequence...)
+		return fitness
+	}
+	return n.fitness[id]
+}
+
+func (n *sequenceNode) NumSites() int {
+	return len(n.sequence)
+}
+
+func (n *sequenceNode) StateCounts() map[int]int {
+	return n.stateCounts
+}
+
+func (n *sequenceNode) History(h [][]int) [][]int {
+	h = append(h, n.sequence)
+	if len(n.parents) == 0 {
+		return h
+	}
+	// TODO: Assumes no recombination. Only follows the first parent
+	return n.parents[0].History(h)
+}
 
 // SequenceTree represents the pathogen as a series of one-hit differences
 // from its ancestor.
 type SequenceTree interface {
 	// Sequence returns a node in the tree.
 	Sequence(id int) (SequenceNode, error)
-	// NewHit creates a new node based on the parent and the
+	// NewSub creates a new node based on the parent and the
 	// position and new state of the substitution.
-	NewHit(parent SequenceNode, position, state int) SequenceNode
+	NewSub(parent SequenceNode, position, state int) SequenceNode
+	// NewRecomb creates a new node given two parents and the position at which recombination occurred.
+	NewRecomb(parent1, parent2 SequenceNode, position int) SequenceNode
 	// NewRoot adds a new root sequence node.
 	NewRoot(id int, sequence []int) (SequenceNode, error)
 }
@@ -122,61 +203,4 @@ func (t *sequenceTree) NewRoot(id int, sequence []int) (SequenceNode, error) {
 	t.pathogens[id] = n
 	t.roots[id] = n
 	return n, nil
-}
-
-// SequenceNode represents a sequence genotype in the sequence tree.
-type SequenceNode interface {
-	// ID returns the unique ID of the pathogen node.
-	ID() int
-	// Parent returns the parent of the node.
-	Parent() SequenceNode
-	// Children returns the children of the node.
-	Children() []SequenceNode
-	// Sequence returns the sequence of the current node.
-	Sequence() []int
-	// Fitness returns the fitness value of this node based on its current
-	// sequence and the given fitness function.
-	Fitness(landscape SequenceFitness) float64
-	// NumSites returns the number of sites being modeled in this pathogen node.
-	NumSites() int
-	// NumStates returns the number of sites by state, postion corresponds to the state from 0 to n.
-	NumStates() map[int]int
-}
-
-type sequenceNode struct {
-	id        int
-	parent    *sequenceNode
-	children  []SequenceNode
-	hits      map[int]int
-	sequence  []int
-	numStates map[int]int
-}
-
-func (n *sequenceNode) ID() int {
-	return n.id
-}
-
-func (n *sequenceNode) Parent() SequenceNode {
-	return n.parent
-}
-
-func (n *sequenceNode) Children() []SequenceNode {
-	return n.children
-}
-
-func (n *sequenceNode) Sequence() []int {
-	return n.sequence
-}
-
-func (n *sequenceNode) Fitness(landscape SequenceFitness) float64 {
-	fitness, _ := landscape.Fitness(n.sequence...)
-	return fitness
-}
-
-func (n *sequenceNode) NumSites() int {
-	return len(n.sequence)
-}
-
-func (n *sequenceNode) NumStates() map[int]int {
-	return n.numStates
 }
