5c9b72c2f17c2340a62953d53a02ae4675b072de
kent <kentkawashima@gmail.com>
2018-05-30 22:55:10 +0900

    Adds GenotypeTree

--- sequence_tree.go
+++ sequence_tree.go
@@ -147,22 +147,33 @@ type GenotypeNode interface {
 	AddChild(child GenotypeNode)
 	// Sequence returns the sequence of the current node.
 	Sequence() []int
+	// CurrentGenotype returns the current genotype of the current node.
+	CurrentGenotype() Genotype
 	// History returns the list of sequences that resulted into the extant
 	// sequence.
 	History(h [][]int) [][]int
+	// Fitness returns the fitness value of this node based on its current
+	// sequence and the given fitness model. If the fitness of the node has
+	// been computed before using the same fitness model, then the value is
+	// returned from memory and is not recomputed.
+	Fitness(f FitnessModel) float64
+	// NumSites returns the number of sites being modeled in this pathogen node.
+	NumSites() int
+	// StateCounts returns the number of sites by state, postion corresponds
+	// to the state from 0 to n.
+	StateCounts() map[int]int
 }
 
 type genotypeNode struct {
 	sync.RWMutex
+	Genotype
 	uid      ksuid.KSUID
 	sequence []int
-	genotype Genotype
 	parents  []GenotypeNode
 	children []GenotypeNode
 }
 
 // NewGenotypeNode creates a new genotype node from a sequence.
-// Automatically adds sequence to the genotypeSet if it is not yet present.
 func NewGenotypeNode(sequence []int, set GenotypeSet, parents ...GenotypeNode) GenotypeNode {
 	genotype := set.AddSequence(sequence)
 
@@ -179,7 +190,7 @@ func NewGenotypeNode(sequence []int, set GenotypeSet, parents ...GenotypeNode) G
 	// Initialize children
 	n.children = []GenotypeNode{}
 	// Assign genotype
-	n.genotype = genotype
+	n.Genotype = genotype
 	// Copy sequence
 	n.sequence = make([]int, len(sequence))
 	copy(n.sequence, sequence)
@@ -215,6 +226,10 @@ func (n *genotypeNode) Sequence() []int {
 	return n.sequence
 }
 
+func (n *genotypeNode) CurrentGenotype() Genotype {
+	return n.Genotype
+}
+
 func (n *genotypeNode) History(h [][]int) [][]int {
 	h = append(h, n.sequence)
 	if len(n.parents) == 0 {
@@ -222,4 +237,65 @@ func (n *genotypeNode) History(h [][]int) [][]int {
 	}
 	// TODO: Assumes no recombination. Only follows the first parent
 	return n.parents[0].History(h)
+}
+
+// GenotypeTree represents the genotypes as a series of differences
+// from its ancestor.
+type GenotypeTree interface {
+	// Set returns the GenotypeSet associated with this tree.
+	Set() GenotypeSet
+	// NewNode creates a new genotype node from a given sequence.
+	// Automatically adds sequence to the genotypeSet if it is not yet present.
+	NewNode(sequence []int, parents ...GenotypeNode) GenotypeNode
+}
+
+type genotypeTree struct {
+	sync.RWMutex
+	genotypes map[ksuid.KSUID]GenotypeNode
+	set       GenotypeSet
+}
+
+// EmptyGenotypeTree creates a new empty genotype tree.
+func EmptyGenotypeTree() GenotypeTree {
+	tree := new(genotypeTree)
+	tree.genotypes = make(map[ksuid.KSUID]GenotypeNode)
+	tree.set = EmptyGenotypeSet()
+	return tree
+}
+
+func (t *genotypeTree) Set() GenotypeSet {
+	return t.set
+}
+
+func (t *genotypeTree) NewNode(sequence []int, parents ...GenotypeNode) GenotypeNode {
+	genotype := t.set.AddSequence(sequence)
+
+	// Create new node
+	n := new(genotypeNode)
+	n.uid = ksuid.New()
+	// Assign its parent
+	if len(parents) > 0 {
+		n.parents = make([]GenotypeNode, len(parents))
+		copy(n.parents, parents)
+	} else {
+		n.parents = []GenotypeNode{}
+	}
+	// Initialize children
+	n.children = []GenotypeNode{}
+	// Assign genotype
+	n.Genotype = genotype
+	// Copy sequence
+	n.sequence = make([]int, len(sequence))
+	copy(n.sequence, sequence)
+
+	// Add new sequence as child of its parent
+	for _, parent := range parents {
+		parent.AddChild(n)
+	}
+
+	// Add to tree map
+	t.Lock()
+	defer t.Unlock()
+	t.genotypes[n.uid] = n
+	return n
 }
