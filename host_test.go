ef9105762353a856d2f8ed32d935b2e4f0d35a9f
kent <kentkawashima@gmail.com>
2018-05-30 18:00:54 +0900

    Adds host_test.go

---
+++ host_test.go
@@ -0,0 +1,59 @@
+package contagiongo
+
+import "testing"
+
+func TestNewEmptySequenceHost(t *testing.T) {
+	id := 1
+	h := NewEmptySequenceHost(id)
+	if h.ID() != id {
+		t.Errorf(UnequalIntParameterError, "host ID", id, h.ID())
+	}
+	if h.TypeID() != 0 {
+		t.Errorf(UnequalIntParameterError, "host type ID", 0, h.TypeID())
+	}
+	if len(h.Pathogens()) > 0 {
+		t.Errorf(UnequalIntParameterError, "number of pathogens", 0, len(h.Pathogens()))
+	}
+
+	id = 1
+	typeID := 1
+	h = NewEmptySequenceHost(id, typeID)
+	if h.ID() != id {
+		t.Errorf(UnequalIntParameterError, "host ID", id, h.ID())
+	}
+	if h.TypeID() != typeID {
+		t.Errorf(UnequalIntParameterError, "host type ID", typeID, h.TypeID())
+	}
+	if len(h.Pathogens()) > 0 {
+		t.Errorf(UnequalIntParameterError, "number of pathogens", 0, len(h.Pathogens()))
+	}
+}
+
+func sampleIntrahostModel() IntrahostModel {
+	model := new(ConstantPopModel)
+	model.mutationRate = 1e-5
+	model.transitionMatrix = [][]float64{
+		[]float64{0, 1},
+		[]float64{1, 0},
+	}
+	model.recombinationRate = 0
+	model.popSize = 1000
+	return model
+}
+
+func TestSequenceHost_SetIntrahostModel(t *testing.T) {
+	id := 1
+	typeID := 1
+	h := NewEmptySequenceHost(id, typeID)
+	seqH := h.(*sequenceHost)
+	model := sampleIntrahostModel()
+
+	if seqH.IntrahostModel != nil {
+		t.Errorf(IntrahostModelExistsError, seqH.IntrahostModel.Name(), seqH.IntrahostModel.ID())
+	}
+	// Add model
+	h.SetIntrahostModel(model)
+	if seqH.IntrahostModel == nil {
+		t.Errorf(EmptyIntrahostModelError)
+	}
+}
