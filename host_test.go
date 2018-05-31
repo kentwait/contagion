22a76af97dcd80c404557743134b69d2920839f2
kent <kentkawashima@gmail.com>
2018-05-31 14:12:34 +0900

    Adds check for setters

--- host_test.go
+++ host_test.go
@@ -2,30 +2,85 @@ package contagiongo
 
 import "testing"
 
-func TestNewEmptySequenceHost(t *testing.T) {
+func TestSequenceHost_Empty_Getters(t *testing.T) {
 	id := 1
-	h := NewEmptySequenceHost(id)
-	if h.ID() != id {
-		t.Errorf(UnequalIntParameterError, "host ID", id, h.ID())
-	}
-	if h.TypeID() != 0 {
-		t.Errorf(UnequalIntParameterError, "host type ID", 0, h.TypeID())
-	}
-	if len(h.Pathogens()) > 0 {
-		t.Errorf(UnequalIntParameterError, "number of pathogens", 0, len(h.Pathogens()))
-	}
-
-	id = 1
 	typeID := 1
-	h = NewEmptySequenceHost(id, typeID)
+	h := NewEmptySequenceHost(id, typeID)
 	if h.ID() != id {
 		t.Errorf(UnequalIntParameterError, "host ID", id, h.ID())
 	}
 	if h.TypeID() != typeID {
 		t.Errorf(UnequalIntParameterError, "host type ID", typeID, h.TypeID())
 	}
-	if len(h.Pathogens()) > 0 {
+	if len(h.Pathogens()) != 0 {
 		t.Errorf(UnequalIntParameterError, "number of pathogens", 0, len(h.Pathogens()))
+	}
+	if h.PathogenPopSize() != 0 {
+		t.Errorf(UnequalIntParameterError, "number of pathogens", 0, h.PathogenPopSize())
+	}
+	if p := h.Pathogen(0); p != nil {
+		t.Errorf(UnequalIntParameterError, "number of pathogens", 0, 1)
+	}
+}
+
+func TestSequenceHost_Infected_Getters(t *testing.T) {
+	id := 1
+	typeID := 0
+	numPathogens := 10
+	sites := 100
+	h := sampleInfectedHost(id, numPathogens, sites)
+	if h.ID() != id {
+		t.Errorf(UnequalIntParameterError, "host ID", id, h.ID())
+	}
+	if h.TypeID() != typeID {
+		t.Errorf(UnequalIntParameterError, "host type ID", typeID, h.TypeID())
+	}
+	if len(h.Pathogens()) != numPathogens {
+		t.Errorf(UnequalIntParameterError, "number of pathogens", 0, len(h.Pathogens()))
+	}
+	if h.PathogenPopSize() != numPathogens {
+		t.Errorf(UnequalIntParameterError, "number of pathogens", 0, h.PathogenPopSize())
+	}
+	if p := h.Pathogen(0); p == nil {
+		t.Errorf(UnequalIntParameterError, "number of pathogens", 1, 0)
+	}
+}
+
+func TestSequenceHost_Setters(t *testing.T) {
+	id := 1
+	typeID := 0
+	numPathogens := 10
+	sites := 100
+	h := sampleInfectedHost(id, numPathogens, sites)
+	if h.ID() != id {
+		t.Errorf(UnequalIntParameterError, "host ID", id, h.ID())
+	}
+	if h.TypeID() != typeID {
+		t.Errorf(UnequalIntParameterError, "host type ID", typeID, h.TypeID())
+	}
+	if len(h.Pathogens()) != numPathogens {
+		t.Errorf(UnequalIntParameterError, "number of pathogens", numPathogens, len(h.Pathogens()))
+	}
+	if h.PathogenPopSize() != numPathogens {
+		t.Errorf(UnequalIntParameterError, "number of pathogens", numPathogens, h.PathogenPopSize())
+	}
+
+	// Add pathogen
+	h.AddPathogen(h.Pathogen(0))
+	if h.PathogenPopSize() != numPathogens+1 {
+		t.Errorf(UnequalIntParameterError, "number of pathogens", numPathogens+1, h.PathogenPopSize())
+	}
+
+	// Remove pathogen
+	h.RemovePathogens(10)
+	if h.PathogenPopSize() != numPathogens {
+		t.Errorf(UnequalIntParameterError, "number of pathogens", numPathogens, h.PathogenPopSize())
+	}
+
+	// Remove all pathogens
+	h.RemoveAllPathogens()
+	if h.PathogenPopSize() != 0 {
+		t.Errorf(UnequalIntParameterError, "number of pathogens", 0, h.PathogenPopSize())
 	}
 }
 
@@ -34,7 +89,7 @@ func TestSequenceHost_SetIntrahostModel(t *testing.T) {
 	typeID := 1
 	h := NewEmptySequenceHost(id, typeID)
 	seqH := h.(*sequenceHost)
-	model := sampleIntrahostModel()
+	model := sampleIntrahostModel(10e-5, 1000)
 
 	if seqH.IntrahostModel != nil {
 		t.Errorf(IntrahostModelExistsError, seqH.IntrahostModel.ModelName(), seqH.IntrahostModel.ModelID())
