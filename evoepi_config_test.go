678c5986a03387261e73ce5f9c78743fed9cbf32
kent <kentkawashima@gmail.com>
2018-05-31 22:39:20 +0900

    Adds single and consecutive NewSimulation tests

--- evoepi_config_test.go
+++ evoepi_config_test.go
@@ -20,17 +20,120 @@ func TestEvoEpiConfig_NewSimulation(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-	sim, err := conf.NewSimulation()
+	sim0, err := conf.NewSimulation()
+	sim := sim0.(*evoEpiSimulation)
 	if err != nil {
 		t.Error(err)
 	}
 
 	// Expected values
 	numHosts := 10
+	infectedHostID := 0
+	pathogenPopSize := 100
+	numNeighbors := 7
 
 	// Tests
+	// Host population
 	if l := len(sim.HostMap()); l != numHosts {
 		t.Errorf(UnequalIntParameterError, "number of hosts", numHosts, l)
+	}
+	// Infected host should only be host 0
+	for i, host := range sim.HostMap() {
+		if host.PathogenPopSize() > 0 && infectedHostID != i {
+			t.Errorf(
+				UnequalIntParameterError,
+				fmt.Sprintf("nubmer of pathogens in host %d", i),
+				0, host.PathogenPopSize(),
+			)
+		}
+	}
+	// Number of pathogens in host 0
+	if l := sim.Host(0).PathogenPopSize(); l != pathogenPopSize {
+		t.Errorf(
+			UnequalIntParameterError,
+			fmt.Sprintf("nubmer of pathogens in host %d", 0),
+			pathogenPopSize, l,
+		)
+	}
+	// Number of neighbors in host 0
+	if l := len(sim.HostNeighbors(0)); l != numNeighbors {
+		t.Errorf(
+			UnequalIntParameterError,
+			fmt.Sprintf("nubmer of neighbors for host %d", 0),
+			numNeighbors, l,
+		)
+	}
+	// Number of intrahost models
+	if l := len(sim.intrahostModels); l != 1 {
+		t.Errorf(UnequalIntParameterError, "number of intrahost models", 1, l)
+	}
+	// Number of fitness models
+	if l := len(sim.fitnessModels); l != 1 {
+		t.Errorf(UnequalIntParameterError, "number of fitness models", 1, l)
+	}
+}
+
+func TestEvoEpiConfig_NewSimulation_NewInstance(t *testing.T) {
+	conf := sampleEvoEpiConfig()
+	err := conf.Validate()
+	if err != nil {
+		t.Error(err)
+	}
+	sim0, err := conf.NewSimulation()
+	if err != nil {
+		t.Error(err)
+	}
+
+	sim1, err := sim0.NewInstance()
+	if err != nil {
+		t.Error(err)
+	}
+
+	// Check is configs have the same pointer
+	sim0ConfigPtr := fmt.Sprintf("%p", sim0.(*evoEpiSimulation).config)
+	sim1ConfigPtr := fmt.Sprintf("%p", sim1.(*evoEpiSimulation).config)
+	if sim0ConfigPtr != sim1ConfigPtr {
+		t.Errorf(
+			NotIdenticalPointerError,
+			"sim0 host 0", sim0.(*evoEpiSimulation).config,
+			"sim1 host 0", sim1.(*evoEpiSimulation).config,
+		)
+	}
+	// Check is simulations have the same pointer
+	sim0Ptr := fmt.Sprintf("%p", sim0)
+	sim1Ptr := fmt.Sprintf("%p", sim1)
+	if sim0Ptr == sim1Ptr {
+		t.Errorf(IdenticalPointerError, "sim0", sim0, "sim1", sim1)
+	}
+	// Check whether hosts have the same pointer
+	sim0HostPtr := fmt.Sprintf("%p", sim0.Host(0))
+	sim1HostPtr := fmt.Sprintf("%p", sim1.Host(0))
+	if sim0HostPtr == sim1HostPtr {
+		t.Errorf(
+			IdenticalPointerError,
+			"sim0 host 0", sim0.Host(0),
+			"sim1 host 0", sim1.Host(0),
+		)
+	}
+	// Check whether trees have the same pointer
+	sim0TreePtr := fmt.Sprintf("%p", sim0.(*evoEpiSimulation).tree)
+	sim1TreePtr := fmt.Sprintf("%p", sim1.(*evoEpiSimulation).tree)
+	if sim0TreePtr == sim1TreePtr {
+		t.Errorf(
+			IdenticalPointerError,
+			"sim0 tree", sim0.(*evoEpiSimulation).tree,
+			"sim1 tree", sim1.(*evoEpiSimulation).tree,
+		)
+	}
+	// Check whether adjacency matrices have the same pointer
+	sim0NetPtr := fmt.Sprintf("%p", sim0.(*evoEpiSimulation).hostNetwork)
+	sim1NetPtr := fmt.Sprintf("%p", sim1.(*evoEpiSimulation).hostNetwork)
+	if sim0NetPtr == sim1NetPtr {
+		t.Errorf(
+			IdenticalPointerError,
+			"sim0 host network", sim0.(*evoEpiSimulation).hostNetwork,
+			"sim1 host network", sim1.(*evoEpiSimulation).hostNetwork,
+		)
 	}
 }
 
