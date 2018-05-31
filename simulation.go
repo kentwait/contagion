772db353d11b67f8c07ae2ebaa699ef2dd8d2bd8
kent <kentkawashima@gmail.com>
2018-05-31 11:39:53 +0900

    Adds Infection and InfectionSimulation

--- simulation.go
+++ simulation.go
@@ -15,10 +15,10 @@ const (
 	VaccinatedStatusCode  = 8
 )
 
-// Simulation encapsulates the set of hosts, its connections,
+// Epidemic encapsulates the set of hosts, its connections,
 // the pathogen tree lineage and the host types used to create
 // a simulated epidemic.
-type Simulation interface {
+type Epidemic interface {
 	// Host returns the selected host in the simulation.
 	Host(id int) Host
 
@@ -75,6 +75,73 @@ type Simulation interface {
 	// DeadProcess performs intrahost processes while the host is in
 	// the dead status.
 	VaccinatedProcess(host Host, wg *sync.WaitGroup)
+}
+
+// EpidemicSimulation is a simulation environment that simulates
+// the spread of disease between hosts in a connected host network.
+type EpidemicSimulation interface {
+	Epidemic
+	// Run runs the whole simulation
+	Run(DataRecorder interface{})
+}
+
+// Infection encapsulates a single host and the pathogen tree lineage
+// to trace the evolution of pathogens within one host. This is useful
+// to strudy intrahost evolutionary dynamics especially in chronic diseases.
+type Infection interface {
+	// Host returns the selected host in the simulation.
+	Host(id int) Host
+
+	// HostStatus retrieves the current status of the selected host.
+	HostStatus(id int) int
+	// SetHostStatus sets the current status of the selected host
+	// to a given status code.
+	SetHostStatus(id, status int)
+	// HostStatusDuration returns the number of generations a host
+	// remains in a given status.
+	HostStatusDuration(id, status int) int
+	// HostTime returns the current number of generations remaining
+	// before the host changes status.
+	HostTimer(id int) int
+	// SetHostTimer sets the number of generations for the host to
+	// remain in its current status.
+	SetHostTimer(id, interval int)
+
+	// The following methods perform intrahost processes associated with
+	// the status. For every generation, one of the following is called for
+	// each host.
+
+	// SusceptibleProcess performs intrahost processes while the host is in
+	// the susceptible status.
+	SusceptibleProcess(host Host, wg *sync.WaitGroup)
+	// ExposedProcess performs intrahost processes while the host is in
+	// the exposed status.
+	ExposedProcess(host Host, wg *sync.WaitGroup)
+	// InfectedProcess performs intrahost processes while the host is in
+	// the infected status.
+	InfectedProcess(host Host, wg *sync.WaitGroup)
+	// InfectiveProcess performs intrahost processes while the host is in
+	// the infective status.
+	InfectiveProcess(host Host, wg *sync.WaitGroup)
+	// RemovedProcess performs intrahost processes while the host is in
+	// the removed status.
+	RemovedProcess(host Host, wg *sync.WaitGroup)
+	// RecoveredProcess performs intrahost processes while the host is in
+	// the recovered status.
+	RecoveredProcess(host Host, wg *sync.WaitGroup)
+	// DeadProcess performs intrahost processes while the host is in
+	// the dead status.
+	DeadProcess(host Host, wg *sync.WaitGroup)
+	// DeadProcess performs intrahost processes while the host is in
+	// the dead status.
+	VaccinatedProcess(host Host, wg *sync.WaitGroup)
+}
+
+// SingleHostSimulation is a simulation environment that simulates
+// the infection within a single hosts or in an evironment where a network
+// configuration is not necessary.
+type SingleHostSimulation interface {
+	Infection
 	// Run runs the whole simulation
 	Run(DataRecorder interface{})
 }
