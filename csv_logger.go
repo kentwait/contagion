c1c3dd91974943b64fdcb296888c0790953fbcc1
kent <kentkawashima@gmail.com>
2018-06-02 02:26:30 +0900

    Updates writers

--- csv_logger.go
+++ csv_logger.go
@@ -3,7 +3,8 @@ package contagiongo
 import (
 	"bytes"
 	"fmt"
-	"sync"
+	"os"
+	"strings"
 )
 
 // CSVLogger is a DataLogger that writes simulation data
@@ -13,54 +14,158 @@ type CSVLogger struct {
 	genotypeNodePath string
 	genotypeFreqPath string
 	statusPath       string
+	transmissionPath string
 	mutationPath     string
 }
 
-func (l *CSVLogger) WriteGenotypes(c <-chan Genotype, wg *sync.WaitGroup) {
-	defer wg.Done()
+func NewCSVLogger(basepath string) *CSVLogger {
+	l := new(CSVLogger)
+	l.SetBasePath(basepath)
+	return l
+}
+
+func (l *CSVLogger) SetBasePath(basepath string) {
+	if info, err := os.Stat(basepath); err == nil && info.IsDir() {
+		basepath += "log"
+	}
+	l.genotypePath = strings.TrimSuffix(basepath, ".") + ".g.csv"
+	l.genotypeNodePath = strings.TrimSuffix(basepath, ".") + ".n.csv"
+	l.genotypeFreqPath = strings.TrimSuffix(basepath, ".") + ".freq.csv"
+	l.statusPath = strings.TrimSuffix(basepath, ".") + ".status.csv"
+	l.transmissionPath = strings.TrimSuffix(basepath, ".") + ".trans.csv"
+	l.mutationPath = strings.TrimSuffix(basepath, ".") + ".tree.csv"
+}
+
+func (l *CSVLogger) WriteGenotypes(c <-chan Genotype) {
 	// Format
 	// <genotypeID>  <sequence>
-	const genotypeRowTemplate = "%s,%s"
-
+	const template = "%s,%s\n"
 	var b bytes.Buffer
+	// b.WriteString("genotypeID,sequence\n")
 	for genotype := range c {
-		row := fmt.Sprintf(genotypeRowTemplate+"\n", genotype.GenotypeUID(), genotype.StringSequence())
+		row := fmt.Sprintf(template,
+			genotype.GenotypeUID(),
+			genotype.StringSequence(),
+		)
 		// TODO: log error
 		b.WriteString(row)
 	}
+	AppendToFile(l.genotypePath, b.Bytes())
 }
 
-func (l *CSVLogger) WriteGenotypeNodes(c <-chan GenotypeNode, wg *sync.WaitGroup) {
+func (l *CSVLogger) WriteGenotypeNodes(c <-chan GenotypeNode) {
 	// Format
 	// <nodeID>  <genotypeID>
-	const nodeRowTemplate = "%s,%s"
-
+	const template = "%s,%s\n"
+	var b bytes.Buffer
+	// b.WriteString("nodeID,genotypeID\n")
+	for node := range c {
+		row := fmt.Sprintf(template,
+			node.UID(),
+			node.GenotypeUID(),
+		)
+		// TODO: log error
+		b.WriteString(row)
+	}
+	AppendToFile(l.genotypeNodePath, b.Bytes())
 }
 
-func (l *CSVLogger) WriteGenotypeFreq(c <-chan GenotypeFreqPackage, wg *sync.WaitGroup) {
+func (l *CSVLogger) WriteGenotypeFreq(c <-chan GenotypeFreqPackage) {
 	// Format
-	// <instanceID>  <generation>  <hostID>  <genotypeID>  <count>
-	const genotypeFreqRowTemplate = "%d,%d,%d,%s,%d"
-
+	// <instanceID>  <generation>  <hostID>  <genotypeID>  <freq>
+	const template = "%d,%d,%d,%s,%d\n"
+	var b bytes.Buffer
+	// b.WriteString("instance,generation,hostID,genotypeID,freq\n")
+	for pack := range c {
+		row := fmt.Sprintf(template,
+			pack.instanceID,
+			pack.genID,
+			pack.hostID,
+			pack.genotypeID.String(),
+			pack.freq,
+		)
+		// TODO: log error
+		b.WriteString(row)
+	}
+	AppendToFile(l.genotypeFreqPath, b.Bytes())
 }
 
-func (l *CSVLogger) WriteMutations(c <-chan MutationPackage, wg *sync.WaitGroup) {
+func (l *CSVLogger) WriteMutations(c <-chan MutationPackage) {
 	// Format
 	// <instanceID>  <generation>  <hostID>  <parentGenotypeID>  <currentGenotypeID>
-	const mutationRowTemplate = "%d,%d,%d,%s,%s"
-
+	const template = "%d,%d,%d,%s,%s\n"
+	var b bytes.Buffer
+	// b.WriteString("instance,generation,hostID,parentGenotypeID,currentGenotypeID\n")
+	for pack := range c {
+		row := fmt.Sprintf(template,
+			pack.instanceID,
+			pack.genID,
+			pack.hostID,
+			pack.parentNodeID.String(),
+			pack.nodeID.String(),
+		)
+		// TODO: log error
+		b.WriteString(row)
+	}
+	AppendToFile(l.mutationPath, b.Bytes())
 }
 
-func (l *CSVLogger) WriteStatus(c <-chan StatusPackage, wg *sync.WaitGroup) {
+func (l *CSVLogger) WriteStatus(c <-chan StatusPackage) {
 	// Format
 	// <instanceID>  <generation>  <hostID>  <status>
-	const statusRowTemplate = "%d,%d,%d,%d"
-
+	const template = "%d,%d,%d,%d\n"
+	var b bytes.Buffer
+	// b.WriteString("instance,generation,hostID,status\n")
+	for pack := range c {
+		row := fmt.Sprintf(template,
+			pack.instanceID,
+			pack.genID,
+			pack.hostID,
+			pack.status,
+		)
+		// TODO: log error
+		b.WriteString(row)
+	}
+	AppendToFile(l.statusPath, b.Bytes())
 }
 
-func (l *CSVLogger) WriteTransmission(c <-chan TransmissionPackage, wg *sync.WaitGroup) {
+func (l *CSVLogger) WriteTransmission(c <-chan TransmissionPackage) {
 	// Format
 	// <instanceID>  <generation>  <fromHostID>  <toHostID> <genotypeID>
-	const transmissionRowTemplate = "%d,%d,%d,%d,%s"
+	const template = "%d,%d,%d,%d,%s\n"
+	var b bytes.Buffer
+	// b.WriteString("instance,generation,fromHostID,toHostID,genotypeID\n")
+	for pack := range c {
+		row := fmt.Sprintf(template,
+			pack.instanceID,
+			pack.genID,
+			pack.fromHostID,
+			pack.toHostID,
+			pack.nodeID.String(),
+		)
+		// TODO: log error
+		b.WriteString(row)
+	}
+	AppendToFile(l.transmissionPath, b.Bytes())
+}
 
+// AppendToFile creates a new file on the given path if it does not exist, or
+// appends to the end of the existing file if the file exists.
+func AppendToFile(path string, b []byte) error {
+	// Create file
+	f, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	_, err = f.Write(b)
+	if err != nil {
+		return err
+	}
+	err = f.Sync()
+	if err != nil {
+		return err
+	}
+	return nil
 }
