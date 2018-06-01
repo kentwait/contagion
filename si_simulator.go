8fbcd2800a7edd405308129b680daa9b591b6899
kent <kentkawashima@gmail.com>
2018-06-01 22:04:05 +0900

    Creates SI simulator

---
+++ si_simulator.go
@@ -0,0 +1,225 @@
+package contagiongo
+
+import (
+	"sync"
+
+	"github.com/segmentio/ksuid"
+)
+
+// import "sync"
+
+// SISimulator creates and runs an SI epidemiological simulation.
+// Within this simulation, hosts may or may not run
+// independent genetic evolution simulations.
+type SISimulator struct {
+	Epidemic
+	DataLogger
+
+	instanceID         int
+	numGenerations     int
+	infectableStatuses []int
+	pathogenLogFreq    int
+	hostLogFreq        int
+}
+
+// Run instantiates, runs, and records the a new simulation.
+func (sim *SISimulator) Run(i int) {
+	sim.instanceID = i
+	sim.Update(0)
+	for t := 1; t < sim.numGenerations; t++ {
+		sim.Process(t)
+		sim.Transmit(t)
+		sim.Update(t)
+	}
+	sim.Finalize()
+}
+
+// Update looks at the timer or internal state to decide if
+// the status of the host remains the same of will change.
+// After the status updates, each host's status is recorded to file.
+func (sim *SISimulator) Update(t int) {
+	// Update status first
+	c := make(chan StatusPackage)
+	d := make(chan GenotypeFreqPackage)
+	var wg sync.WaitGroup
+	// Read all hosts and process hosts concurrently
+	// These succeeding steps connects the simulation's record of
+	// each host's status and timer with the host's internal state.
+	for hostID, host := range sim.HostMap() {
+		// Simulation-level record of status and timer of particular host
+		timer := sim.HostTimer(hostID)
+		pack := StatusPackage{
+			instanceID: sim.instanceID,
+			genID:      t,
+			hostID:     hostID,
+			status:     sim.HostStatus(hostID), // current host status before checking
+		}
+		wg.Add(1)
+		go func(i, t int, host Host, timer int, pack StatusPackage, c chan<- StatusPackage, d chan<- GenotypeFreqPackage, wg *sync.WaitGroup) {
+			defer wg.Done()
+			// Add cases depending on the compartmental model being used
+			// In this case, SI only uses susceptible and infected statuses
+			switch pack.status {
+			case SusceptibleStatusCode:
+				// Use timer or number of pathogens
+				if timer == 0 || host.PathogenPopSize() > 0 {
+					// Set new host status
+					newStatus := InfectedStatusCode
+					newDuration := host.GetIntrahostModel().StatusDuration(newStatus)
+					sim.SetHostStatus(host.ID(), newStatus)
+					sim.SetHostTimer(host.ID(), newDuration)
+					// Update status in pack and send
+					pack.status = newStatus
+					c <- pack
+				}
+			default:
+				// No change
+				// Just send pack
+				c <- pack
+			}
+			// Record pathogen frequencies
+			counts := make(map[ksuid.KSUID]int)
+			for _, p := range host.Pathogens() {
+				counts[p.CurrentGenotype().GenotypeUID()]++
+			}
+			for uid, freq := range counts {
+				d <- GenotypeFreqPackage{
+					instanceID: i,
+					genID:      t,
+					hostID:     host.ID(),
+					genotypeID: uid,
+					freq:       freq,
+				}
+			}
+		}(sim.instanceID, t, host, timer, pack, c, d, &wg)
+	}
+	go func() {
+		wg.Wait()
+		close(c)
+		close(d)
+	}()
+	// Write status  and genotype frequencies using DataLogger
+	var wg2 sync.WaitGroup
+	wg2.Add(2)
+	sim.WriteStatus(c)
+}
+
+// Process runs the internal evolution simulation in each host.
+// During intrahost evolution, if new mutations appear, the new sequence
+// and ancestry is recorded to file.
+func (sim *SISimulator) Process(t int) {
+	c := make(chan MutationPackage)
+	var wg sync.WaitGroup
+	// Read all hosts and process based on the current status of the host
+	for hostID, host := range sim.HostMap() {
+		// Run the intrahost process asynchronously depending on the
+		// current status of the host.
+		wg.Add(1)
+		switch sim.HostStatus(hostID) {
+		case SusceptibleStatusCode:
+			go sim.SusceptibleProcess(sim.instanceID, t, host, &wg)
+		case InfectedStatusCode:
+			go sim.InfectedProcess(sim.instanceID, t, host, c, &wg)
+		}
+		// Decrement host timer.
+		// If status depends on timer, then timer will go positive integer
+		// to 0.
+		// If status is not dependent on timer, then will just decrement
+		// from -1 to more negative values
+		sim.SetHostTimer(hostID, sim.HostTimer(hostID)-1)
+	}
+	go func() {
+		wg.Wait()
+		close(c)
+	}()
+	// Write mutations to DataLogger
+	sim.WriteMutations(c)
+}
+
+// Transmit facilitates the sampling and migration process of pathogens
+// between hosts.
+func (sim *SISimulator) Transmit(t int) {
+	c := make(chan TransmissionEvent)
+	d := make(chan TransmissionPackage)
+	var wg sync.WaitGroup
+	// Get hosts that are infected and determine pathogen pop size
+	// Only hosts with an infected status can transmit
+	var infectedHosts []Host
+	var pathogenPopSizes []int
+	for hostID, host := range sim.HostMap() {
+		if sim.HostStatus(hostID) == InfectedStatusCode {
+			infectedHosts = append(infectedHosts, host)
+			pathogenPopSizes = append(pathogenPopSizes, host.PathogenPopSize())
+		}
+	}
+	// Iterate using pre-assembled list of infected hosts
+	for i, host := range infectedHosts {
+		// Iterate over host's neighbors and create a new goroutine
+		// that determines whether pathogens transmit or not
+		hostID := host.ID()
+		count := pathogenPopSizes[i]
+		for _, neighbor := range sim.HostNeighbors(hostID) {
+			status := sim.HostStatus(neighbor.ID())
+			for _, infectableStatus := range sim.InfectableStatuses() {
+				if status == infectableStatus {
+					wg.Add(1)
+					go TransmitPathogens(sim.instanceID, t, host, neighbor, count, status, c, d, &wg)
+				}
+			}
+
+		}
+	}
+	go func() {
+		wg.Wait()
+		close(c)
+		close(d)
+	}()
+	// Add the new pathogen to the destination host
+	// and record
+	var wg2 sync.WaitGroup
+	wg2.Add(2)
+	go func() {
+		for t := range c {
+			t.destination.AddPathogen(t.pathogen)
+		}
+		wg2.Done()
+	}()
+	go func() {
+		sim.WriteTransmission(d)
+		wg2.Done()
+	}()
+	wg2.Wait()
+}
+
+// Finalize performs processes to finish and close the simulation.
+func (sim *SISimulator) Finalize() {
+	// Record genotype tree
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go func(wg *sync.WaitGroup) {
+		c := make(chan GenotypeNode)
+
+		for _, node := range sim.GenotypeNodeMap() {
+			c <- node
+		}
+		close(c)
+		sim.WriteGenotypeNodes(c)
+		wg.Done()
+	}(&wg)
+	go func(wg *sync.WaitGroup) {
+		d := make(chan Genotype)
+		for _, genotype := range sim.GenotypeSet().Map() {
+			d <- genotype
+		}
+		close(d)
+		sim.WriteGenotypes(d)
+		wg.Done()
+	}(&wg)
+	// Block until finished
+	wg.Wait()
+
+	// Clear memory by deleting pathogens in host
+	for _, host := range sim.HostMap() {
+		host.RemoveAllPathogens()
+	}
+}
