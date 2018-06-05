943cb7da24550051e34425fd3871c2cc6050505c
kent <kentkawashima@gmail.com>
2018-06-05 18:43:20 +0900

    Adds docstring

--- configurator/__init__.py
+++ configurator/__init__.py
@@ -1,3 +1,28 @@
+"""
+The configurator module contains classes and functions related to
+running the command-prompt interface in contagion.py.
+
+The module is divided into three main files:
+- validators.py
+- handlers.py
+- configuration.py
+
+Validation of input statements are performed by StatementValidator
+an instance of the Validator class in prompt_toolkit. Depending on
+the command, StatementValidator calls specific sub-validators in
+validators.py to handle validating a particular command for
+formatting and value.
+
+Commands are executed its respective handler function in handlers.py.
+The handler function may prompt for more input and validate the given
+data as well.
+
+Finally, configuration.py holds the Configuration class. The Configuration
+class holds the settings and parameters to create and run the simulation.
+When ready, the Configuration class can write the settings into a TOML
+file that can be read by Contagion. It can also import settings from an
+existing TOML file.
+"""
 from configurator.configuration import Configuration
 from configurator import handlers
 
