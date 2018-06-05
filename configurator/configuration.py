66fab4d982ef31f77669c3162fce6770b14c2782
kent <kentkawashima@gmail.com>
2018-06-05 19:25:10 +0900

    Adds new_intrahost_model_prompt method to Configuration

--- configurator/configuration.py
+++ configurator/configuration.py
@@ -1,4 +1,6 @@
 from collections import OrderedDict
+from prompt_toolkit import prompt
+from prompt_toolkit.contrib.completers import WordCompleter
 
 
 class Configuration(object):
@@ -76,6 +78,123 @@ class Configuration(object):
     def save(self):
         with open(self.config_path, 'w') as f:
             print(self.toml_string(), file=f)
+
+    def new_intrahost_model_prompt(self, history=None):
+        """Creates a new intrahost_model object.
+
+        Parameters
+        ----------
+        history : prompt_toolkit.history.InMemoryHistory
+
+        Returns
+        -------
+        configurator.configuration.IntrahostModel
+
+        """
+        # Initialize model
+        model = IntrahostModel()
+
+        # Add properties
+        # TODO: create name validator
+        model.model_name = prompt(
+            'Model name: ',
+            history=history,
+            validator=None,
+        )
+        host_ids = host_ids = prompt(
+            'Host IDs: ',
+            history=history,
+            validator=None,
+        )
+        model.host_ids = parse_host_ids(host_ids)
+        # Parameters
+        model.mutation_rate = float(
+            prompt('Mutation rate (subs/site/generation): ',
+                   validator=None)
+        )
+        transition_matrix = prompt(
+            'Conditioned transition rate matrix: ',
+            validator=None,
+        )
+        model.transition_matrix = parse_transition_matrix(transition_matrix)
+        model.recombination_rate = float(
+            prompt('Recombination rate (recombinations/generation): ',
+                   validator=None)
+        )
+        replication_model = prompt(
+            'Replication model: ',
+            validator=None,
+            completer=WordCompleter(['constant', 'bht', 'fitness']),
+        )
+        # Model-dependent parameters
+        if str(replication_model).lower() == 'constant':
+            model.constant_pop_size = int(
+                prompt('Population size: ',
+                       validator=None)
+            )
+        elif str(replication_model).lower() == 'bht':
+            model.max_pop_size = int(
+                prompt('Maximum population size: ',
+                       validator=None)
+            )
+            model.growth_rate = float(
+                prompt('Growth rate: ',
+                       validator=None)
+            )
+        elif str(replication_model).lower() == 'fitness':
+            model.max_pop_size = int(
+                prompt('Maximum population size: ',
+                       validator=None)
+            )
+        # Durations
+        if replication_model != 'fitness':
+            # Check if epidemic model has been set
+            if not self.epidemic_model:
+                # Set epidemic model
+                pass
+            if self.epidemic_model == 'endtrans':
+                model.infected_duration = int(
+                    prompt('Duration at infected status: ',
+                           validator=None)
+                )
+                model.removed_duration = int(
+                    prompt('Duration at removed status: ',
+                           validator=None)
+                )
+            elif self.epidemic_model == 'exchange':
+                pass
+            else:
+                if self.epidemic_model.startswith('se'):
+                    model.exposed_duration = int(
+                        prompt('Duration at exposed status: ',
+                               validator=None)
+                    )
+                if self.epidemic_model.startswith('si'):
+                    model.infected_duration = int(
+                        prompt('Duration at infected status: ',
+                               validator=None)
+                    )
+                if self.epidemic_model.startswith('sei'):
+                    model.infective_duration = int(
+                        prompt('Duration at infective status: ',
+                               validator=None)
+                    )
+                if self.epidemic_model.endswith('r'):
+                    model.removed_duration = int(
+                        prompt('Duration at removed status: ',
+                               validator=None)
+                    )
+                if self.epidemic_model.endswith('rs'):
+                    model.recovered_duration = int(
+                        prompt('Duration at recovered status: ',
+                               validator=None)
+                    )
+                # vaccinated_duration = int(
+                #     prompt('Duration at vaccinated status: ',
+                #            validator=None)
+                # )
+        return model
+
 
 
 class Model(object):
@@ -163,7 +282,7 @@ class FitnessModel(Model):
         ])
         for k,v in _param_dict.items():
             config_string += "{k} = {v}\n".format(k=k, v=v)
-        config_string += '\n'   
+        config_string += '\n'
 
 
 class TransmissionModel(Model):
@@ -184,3 +303,10 @@ class TransmissionModel(Model):
         for k,v in _param_dict.items():
             config_string += "{k} = {v}\n".format(k=k, v=v)
         config_string += '\n' 
+
+
+def parse_host_ids(ids):
+    return []
+
+def parse_transition_matrix(matrix):
+    return []
